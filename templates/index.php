ad1fce76c400de6086e10c71ea1bd04909450307
Manuel Leduc <manuel.leduc@xwiki.com>
2024-05-22 14:09:56 +0200

    --wip-- [skip ci]

---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use OCP\Util;
+
+Util::addScript(OCA\Cristal\AppInfo\Application::APP_ID, 'cristal.umd');
+
+?>
+
+<div id="app"></div>
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	define: {
+		"process.env": {},
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, './src/main.js'),
+			name: 'nexcloudcristal',
+		},
+		outDir: 'js'
+	},
+	plugins: [
+		vue(),
+	],
+	optimizeDeps: {
+		esbuildOptions: {
+			tsconfigRaw: {
+				compilerOptions: {
+					// Workaround for a vite bug (see https://github.com/vitejs/vite/issues/13736)
+					experimentalDecorators: true,
+				},
+			},
+		},
+	},
+})
